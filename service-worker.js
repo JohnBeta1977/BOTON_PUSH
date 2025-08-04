00931549971831e2e25d825cf7c05deded648863
JohnBeta1977 <johnarleybetancur@gmail.com>
2025-08-04 15:32:08 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,55 @@
-document.getElementById('send-notification').addEventListener('click', () => {
+//document.getElementById('send-notification').addEventListener('click', () => {
     // 1. Solicitar permiso de notificación
-    if ('Notification' in window) {
-        Notification.requestPermission(permission => {
-            if (permission === 'granted') {
-                console.log('Permiso de notificación concedido.');
+  //  if ('Notification' in window) {
+    //    Notification.requestPermission(permission => {
+      //      if (permission === 'granted') {
+        //        console.log('Permiso de notificación concedido.');
 
                 // 2. Enviar la notificación
-                const notificationTitle = 'Hola desde mi PWA!';
-                const notificationOptions = {
-                    body: 'Esta es una notificación de prueba manual.',
-                    icon: 'icon.png' // Asegúrate de tener un icono
-                };
+          //      const notificationTitle = 'Hola desde mi PWA!';
+            //    const notificationOptions = {
+              //      body: 'Esta es una notificación de prueba manual.',
+                //    icon: 'icon.png' // Asegúrate de tener un icono
+                //};
 
                 // Uso del service worker para mostrar la notificación
-                navigator.serviceWorker.ready.then(registration => {
-                    registration.showNotification(notificationTitle, notificationOptions);
-                });
+//                navigator.serviceWorker.ready.then(registration => {
+  //                  registration.showNotification(notificationTitle, notificationOptions);
+    //            });
+//
+  //          } else {
+    //            console.log('Permiso de notificación denegado.');
+      //      }
+//        });
+  //  } else {
+    //    console.log('Las notificaciones no son compatibles en este navegador.');
+   // }
+//});
+//self.addEventListener('install', event => {
+  //  console.log('Service Worker instalado.');
+    //self.skipWaiting();
+//});
 
-            } else {
-                console.log('Permiso de notificación denegado.');
-            }
-        });
-    } else {
-        console.log('Las notificaciones no son compatibles en este navegador.');
-    }
-});
+//self.addEventListener('activate', event => {
+    //console.log('Service Worker activado.');
+  //  event.waitUntil(self.clients.claim());
+//});
+
 self.addEventListener('install', event => {
     console.log('Service Worker instalado.');
-    self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
     console.log('Service Worker activado.');
-    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('notificationclick', event => {
     console.log('Notificación clicada:', event);
     event.notification.close();
+});
+
+self.addEventListener('notificationclick', event => {
+    console.log('Notificación clicada:', event);
+    event.notification.close();
+
 });
