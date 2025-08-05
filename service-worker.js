11bc1bd79c48b7db243faa2afddc2ad8e8cf5d67
JohnBeta1977 <johnarleybetancur@gmail.com>
2025-08-04 20:59:30 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,55 +1,15 @@
-//document.getElementById('send-notification').addEventListener('click', () => {
-    // 1. Solicitar permiso de notificación
-  //  if ('Notification' in window) {
-    //    Notification.requestPermission(permission => {
-      //      if (permission === 'granted') {
-        //        console.log('Permiso de notificación concedido.');
-
-                // 2. Enviar la notificación
-          //      const notificationTitle = 'Hola desde mi PWA!';
-            //    const notificationOptions = {
-              //      body: 'Esta es una notificación de prueba manual.',
-                //    icon: 'icon.png' // Asegúrate de tener un icono
-                //};
-
-                // Uso del service worker para mostrar la notificación
-//                navigator.serviceWorker.ready.then(registration => {
-  //                  registration.showNotification(notificationTitle, notificationOptions);
-    //            });
-//
-  //          } else {
-    //            console.log('Permiso de notificación denegado.');
-      //      }
-//        });
-  //  } else {
-    //    console.log('Las notificaciones no son compatibles en este navegador.');
-   // }
-//});
-//self.addEventListener('install', event => {
-  //  console.log('Service Worker instalado.');
-    //self.skipWaiting();
-//});
-
-//self.addEventListener('activate', event => {
-    //console.log('Service Worker activado.');
-  //  event.waitUntil(self.clients.claim());
-//});
-
-self.addEventListener('install', event => {
-    console.log('Service Worker instalado.');
+self.addEventListener('install', () => {
+  console.log('Service Worker instalado');
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-    console.log('Service Worker activado.');
+self.addEventListener('activate', () => {
+  console.log('Service Worker activado');
 });
 
 self.addEventListener('notificationclick', event => {
-    console.log('Notificación clicada:', event);
-    event.notification.close();
-});
-
-self.addEventListener('notificationclick', event => {
-    console.log('Notificación clicada:', event);
-    event.notification.close();
-
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('./') // redirige al inicio
+  );
 });
