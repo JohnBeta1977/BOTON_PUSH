b0b34765014587b9cf63878c14aff0691bdab691
JohnBeta1977 <johnarleybetancur@gmail.com>
2025-08-08 17:02:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Importar los módulos necesarios de Firebase
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 // Inicializar la aplicación de Firebase Admin
 admin.initializeApp();
@@ -8,14 +8,15 @@ const db = admin.firestore();
 
 // Tu clave VAPID privada (¡IMPORTANTE: mantenla segura, nunca la compartas!)
 // Asegúrate de reemplazar 'TU_CLAVE_VAPID_PRIVADA_AQUÍ' con tu clave real.
-// Puedes encontrarla en la misma sección de Firebase Cloud Messaging donde generaste la clave pública.
-const VAPID_PRIVATE_KEY = 'BInO8Dg-awsarYdPs6eFZNO4sgOCNbHkJkuFHzLt25xPcDAjGLwrKwUwW5ci-5IqKNSvOoSmji5U3G9bMbobG3M';
+// Puedes encontrarla en la misma sección de Firebase Cloud Messaging donde
+// generaste la clave pública.
+const VAPID_PRIVATE_KEY = "BInO8Dg-awsarYdPs6eFZNO4sgOCNbHkJkuHHzLt25xPcDAjGLwrKwUwW5ci-5IqKNSvOoSmji5U3G9bMbobG3M";
 
 // Configurar web-push
-const webpush = require('web-push');
+const webpush = require("web-push");
 webpush.setVapidDetails(
-    'mailto:viviraplicaciones@gmail.com', // Reemplaza con tu correo electrónico
-    'BInO8Dg-awsarYdPs6eFZNO4sgOCNbHkJkuFHzLt25xPcDAjGLwrKwUwW5ci-5IqKNSvOoSmji5U3G9bMbobG3M', // Tu clave VAPID pública
+    "mailto:viviraplicaciones@gmail.com", // Reemplaza con tu correo electrónico
+    "BInO8Dg-awsarYdPs6eFZNO4sgOCNbHkJkuHHzLt25xPcDAjGLwrKwUwW5ci-5IqKNSvOoSmji5U3G9bMbobG3M", // Tu clave VAPID pública
     VAPID_PRIVATE_KEY // Tu clave VAPID privada
 );
 
@@ -23,42 +24,48 @@ webpush.setVapidDetails(
 exports.subscribe = functions.https.onRequest(async (req, res) => {
     try {
         const subscription = req.body;
-        await db.collection('subscriptions').add(subscription);
-        res.status(200).send({ message: 'Subscription saved successfully.' });
+        await db.collection("subscriptions").add(subscription);
+        res.status(200).send({message: "Subscription saved successfully."});
     } catch (error) {
-        console.error('Error saving subscription:', error);
-        res.status(500).send({ error: 'Internal Server Error' });
+        console.error("Error saving subscription:", error);
+        res.status(500).send({error: "Internal Server Error"});
     }
 });
 
 // Función programada para enviar la notificación push
 // Se ejecutará todos los días a la 1:30 PM (UTC)
-exports.sendDailyPushNotification = functions.pubsub.schedule('00 14 * * *').onRun(async (context) => {
-    console.log('Running daily push notification job at 2:00 PM UTC');
+exports.sendDailyPushNotification = functions.pubsub
+    .schedule("30 13 * * *")
+    .onRun(async (context) => {
+        console.log("Running daily push notification job at 1:30 PM UTC");
 
-    const payload = JSON.stringify({
-        notification: {
-            title: 'Recordatorio de salud integral',
-            body: '¡No olvides tomar tu medicina a tiempo!',
-            icon: '/BOTON_PUSH/plant-leaf.png'
-        }
-    });
-
-    try {
-        const subscriptionsSnapshot = await db.collection('subscriptions').get();
-        subscriptionsSnapshot.forEach(async doc => {
-            const subscription = doc.data();
-            try {
-                await webpush.sendNotification(subscription, payload);
-            } catch (error) {
-                console.error('Failed to send notification to a subscription:', error);
-            }
+        const payload = JSON.stringify({
+            notification: {
+                title: "Recordatorio de salud integral",
+                body: "¡No olvides tomar tu medicina a tiempo!",
+                icon: "/BOTON_PUSH/plant-leaf.png",
+            },
         });
 
-        console.log('Daily notifications sent successfully.');
-        return null;
-    } catch (error) {
-        console.error('Error sending daily notifications:', error);
-        return null;
-    }
-});
+        try {
+            const subscriptionsSnapshot =
+                await db.collection("subscriptions").get();
+            subscriptionsSnapshot.forEach(async (doc) => {
+                const subscription = doc.data();
+                try {
+                    await webpush.sendNotification(subscription, payload);
+                } catch (error) {
+                    console.error(
+                        "Failed to send notification to a subscription:",
+                        error,
+                    );
+                }
+            });
+
+            console.log("Daily notifications sent successfully.");
+            return null;
+        } catch (error) {
+            console.error("Error sending daily notifications:", error);
+            return null;
+        }
+    });
